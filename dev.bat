ff4593ac007a8b3774c9f2b3643654073127b1de
design7 <min.kim9914@gmail.com>
2026-01-14 16:21:20 +0900

    0

---
+++ dev.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Building...
+call gradlew.bat assembleDebug
+
+echo Installing...
+adb install -r app\build\outputs\apk\debug\app-debug.apk
+
+echo Launching...
+adb shell am start -n com.kyad.traystorage/.app.splash.SplashActivity
+
+echo Logs:
+adb logcat -c
+adb logcat | findstr "TrayStorage OCR ChatBot"
